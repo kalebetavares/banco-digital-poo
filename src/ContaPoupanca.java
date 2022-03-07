3ebf90fde8e657d0ff4cff25a265d7f0a5ab6e46
kalebetavares <kalebe.tavares@gmail.com>
2022-03-07 16:34:27 -0300

    Conceito de Encapsulamento

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,10 +1,11 @@
 
 public class ContaPoupanca {
 
-	public int agencia;
-	public int numero;
-	public double saldo;
+	private int agencia;
+	private int numero;
+	private double saldo;
 	
+
 	public void sacar() {
 		
 	}
@@ -15,5 +16,17 @@ public class ContaPoupanca {
 	
 	public void transferir() {
 		
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public double getSaldo() {
+		return saldo;
 	}
 }
